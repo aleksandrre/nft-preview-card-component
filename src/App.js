56c6b297f1fd9abc634bc065b0a1962b57d4a08e
aleksandrre <alekochokheli.01@gmail.com>
2023-03-04 16:16:49 +0400

    nft-preview-card-component

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import "./reset.css";
+function App() {
+  return (
+    <div className="div1">
+      <div className="div2">
+        <div className="div3">
+          <img className="hidden" src="./img/ayeicon.svg" alt="view-icon" />
+          <div class="hiddendiv"></div>
+          <img className="mainpicture" src="./img/img1.jpg" />
+          <h1>Equilibrium#3429</h1>
+          <p>Our equilibrium collection promotes balance and calm</p>
+          <div className="div4">
+            <div className="div41">
+              <img src="./img/img3.svg" />
+              <h2>ETH</h2>
+            </div>
+            <div className="div42">
+              <img src="./img/img2.svg" />
+              <p>3 days left</p>
+            </div>
+          </div>
+          <div className="div5"></div>
+          <div className="div6">
+            <img src="./img/img5.png" />
+            <h3>Creation of</h3>
+            <h4>Jules Wyvern</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
